9f24dd482859d689c257ae1d179030ef3f6f230a
Ross Buggins <20972856+rossbuggins@users.noreply.github.com>
2021-05-22 19:01:55 +0100

    Added some custom behavior to working.

--- src/MyWorker.cs
+++ src/MyWorker.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace switcher
 {
@@ -11,10 +13,12 @@ namespace switcher
         private readonly Switchers _switchers;
         private readonly MyLightWiringFactory _factory;
 
-        public MyWorker(MyLightWiringFactory factory)
+        private ILogger<MyWorker> _logger;
+        public MyWorker(MyLightWiringFactory factory, ILogger<MyWorker> logger)
         {
             _switchers = new Switchers();
             _factory = factory;
+            _logger = logger;
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
@@ -26,8 +30,23 @@ namespace switcher
         private void SetupRequiredLights()
         {
             var x = _factory.Create<MyTimerLightWiring>();
-            x.Timer.AddOnTime(new TimeSpanRange(TimeSpan.Zero, new TimeSpan(23,59,59)));
+            x.Timer.AddOnTime(new TimeSpanRange(TimeSpan.Zero, new TimeSpan(1,59,59)));
             _switchers.Add(x.Switcher);
+
+            var whatItShouldBe = x.Timer.ShouldBeOnProvider ;
+            
+            //This is showing the customisability of the system, its overriding 
+            //default behaviour in a functional way.
+            x.Timer.ShouldBeOnProvider = (p, o) =>
+            {
+                var onRulesCount = o.Count();
+                var shouldBeRule = whatItShouldBe(p,o);
+                var n = p.UtcNow().TimeOfDay;
+                var state = n.Seconds % 2 ==0;
+                _logger.LogInformation("There are {ruleCount} rules with a result of {plannedResult} but I'm ignoring them and setting the switch to what I want which is {state}.", onRulesCount, shouldBeRule, state);
+               return state;
+            };
+
             x.Enable();
         }
 
